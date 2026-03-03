f8ef4d086b92cb0b3ba1785d778c40958f7de1d1
Krzysiek Jagiełło <jagiellokrzysiek876@gmail.com>
2026-03-03 19:17:44 +0000

    feat: expand gz_bridge capabilities

--- src/sitl_launcher/launch/bridge.launch.py
+++ src/sitl_launcher/launch/bridge.launch.py
@@ -8,8 +8,23 @@ def generate_launch_description():
             executable='parameter_bridge',
             name='gz_bridge',
             arguments=[
+                # Sim time
+                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+
+                # Sensor data: RGBD camera
                 '/camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
-                '/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo'
+                '/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+
+                # Joint states
+                '/world/iris_runway/model/iris_depth/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model',
+
+                # Odometry
+                '/model/iris_depth/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry'
+
+            ],
+            remappings=[
+                ('/world/iris_runway/model/iris_depth/joint_state', '/joint_states'),
+                ('/model/iris_depth/odometry', '/odom')
             ],
             output='screen'
         )
